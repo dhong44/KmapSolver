269b6fe0ca7e97ac65760bf930cbbd3025359e73
Daniel Hong <daniel.hong@live.com>
2017-10-08 19:56:18 -0400

    Remove 'KmapSolver' from url

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import './style.css';
 import 'bootstrap-css';
 import m from 'mithril';
 
-m.route(document.body, "/KmapSolver/4", {
-  "/KmapSolver/2": Variables(2),
-  "/KmapSolver/3": Variables(3),
-  "/KmapSolver/4": Variables(4),
-  "/KmapSolver/5": Variables(5),
-  "/KmapSolver/6": Variables(6),
-  "/KmapSolver/7": Variables(7),
-  "/KmapSolver/8": Variables(8)
+
+m.route(document.body, "/4", {
+  "/2": Variables(2),
+  "/3": Variables(3),
+  "/4": Variables(4),
+  "/5": Variables(5),
+  "/6": Variables(6),
+  "/7": Variables(7),
+  "/8": Variables(8)
 });
--- src/views/Navbar.js
+++ src/views/Navbar.js
@@ -5,31 +5,31 @@ let Navbar = {
     return <nav>
       <ul class="nav">
         <li class="nav-item">
-          <a href="/KmapSolver/2" oncreate={m.route.link} class="nav-link"> 2 variables</a>
+          <a href="/2" oncreate={m.route.link} class="nav-link"> 2 variables</a>
         </li>
 
         <li class="nav-item">
-          <a href="/KmapSolver/3" oncreate={m.route.link} class="nav-link"> 3 variables</a>
+          <a href="/3" oncreate={m.route.link} class="nav-link"> 3 variables</a>
         </li>
 
         <li class="nav-item">
-          <a href="/KmapSolver/4" oncreate={m.route.link} class="nav-link"> 4 variables</a>
+          <a href="/4" oncreate={m.route.link} class="nav-link"> 4 variables</a>
         </li>
 
         <li class="nav-item">
-          <a href="/KmapSolver/5" oncreate={m.route.link} class="nav-link"> 5 variables</a>
+          <a href="/5" oncreate={m.route.link} class="nav-link"> 5 variables</a>
         </li>
 
         <li class="nav-item">
-          <a href="/KmapSolver/6" oncreate={m.route.link} class="nav-link"> 6 variables</a>
+          <a href="/6" oncreate={m.route.link} class="nav-link"> 6 variables</a>
         </li>
 
         <li class="nav-item">
-          <a href="/KmapSolver/7" oncreate={m.route.link} class="nav-link"> 7 variables</a>
+          <a href="/7" oncreate={m.route.link} class="nav-link"> 7 variables</a>
         </li>
 
         <li class="nav-item">
-          <a href="/KmapSolver/8" oncreate={m.route.link} class="nav-link"> 8 variables</a>
+          <a href="/8" oncreate={m.route.link} class="nav-link"> 8 variables</a>
         </li>
       </ul>
     </nav>
