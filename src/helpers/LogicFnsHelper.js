8f5f07ca727e64cd702838bd26120cd90770f361
Daniel Hong <daniel.hong@live.com>
2017-10-07 21:09:04 -0400

    Change truth table to count up in binary

--- src/helpers/LogicFnsHelper.js
+++ src/helpers/LogicFnsHelper.js
@@ -1,7 +1,16 @@
 import grayCode from './GrayCodeHelper';
 
+let createInputs = function(n, inputs){
+  for (let i = 0; i < Math.pow(2, n); i++){
+    inputs.push(("00000000" + (i >>> 0).toString(2)).slice(-n));
+  }
+};
+
 let createLogicFns = function(variables){
-  return grayCode(variables).map(input => {
+  let inputs = [];
+  createInputs(variables, inputs);
+
+  return inputs.map(input => {
     return {
       input,
       output: '0'
